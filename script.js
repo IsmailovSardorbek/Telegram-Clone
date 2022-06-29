972de853ec496b9d4f22aed482ea1874514f0b06
IsmailovSardorbek <105179631+IsmailovSardorbek@users.noreply.github.com>
2022-06-29 14:24:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const users = document.querySelectorAll(".friend-drawer--onhover");
+
+const personName = document.getElementById("person-name");
+
+const profileAvatar = document.querySelector("#avatar");
+
+const lastSeen = document.getElementById("last-seen");
+
+const chatBubbles = document.querySelectorAll(".chat-bubble");
+let people = [
+  { name: "Robo Cop", info: `Last seen 2 hours ago` },
+  { name: "Optimus", info: `Last seen recently` },
+  { name: "Skynet", info: `Last seen just now` },
+  { name: "Termy", info: `Last seen 5 minut es ago` },
+  { name: "Richard", info: `Last seen a long time ago` },
+  { name: "XXXXX", info: `Last seen 1 hour ago` },
+];
+
+let images = [
+  "https://www.clarity-enhanced.net/wp-content/uploads/2020/06/robocop.jpg",
+  "https://www.clarity-enhanced.net/wp-content/uploads/2020/06/optimus-prime.jpeg",
+  "https://www.clarity-enhanced.net/wp-content/uploads/2020/06/real-terminator.png",
+  "https://www.clarity-enhanced.net/wp-content/uploads/2020/06/termy.jpg",
+  "https://www.clarity-enhanced.net/wp-content/uploads/2020/06/rick.jpg",
+  "https://www.clarity-enhanced.net/wp-content/uploads/2020/06/rachel.jpeg",
+];
+
+users.forEach((user, index) => {
+  user.addEventListener("click", function () {
+    let activeUser = document.querySelector(".active-user");
+    activeUser.className = activeUser.className.replace(" active-user", "");
+
+    this.classList.add("active-user");
+    profileAvatar.src = images[index];
+
+    personName.textContent = people[index].name;
+    lastSeen.textContent = people[index].info;
+
+    chatBubbles.forEach((bubble) => {
+      bubble.classList.toggle("fade-in");
+    });
+  });
+});
